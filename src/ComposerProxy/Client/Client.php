b216a886bd4a5401afa493ebad6ac8a69ef2e800
Tom Rochette <roctom@gmail.com>
2014-12-21 19:02:37 -0500

    - Added a hack to load autoload.files from composer.json as ReactPHP depends on some of these files, which Guzzle depends on (our HTTP client to send the required packages to our proxy). - The client can now send the required packages to multiple proxies.

--- src/ComposerProxy/Client/Client.php
+++ src/ComposerProxy/Client/Client.php
@@ -5,6 +5,7 @@ namespace ComposerProxy\Client;
 use Arry\Arry;
 use Composer\Script\Event;
 use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Pool;
 
 class Client
 {
@@ -12,6 +13,17 @@ class Client
 	{
 		$composer = $event->getComposer();
 		$io = $event->getIO();
+
+		// TODO: Remove this hack when composer supports loading autoload.files for plugins
+		$vendorDir = $composer->getConfig()->get('vendor-dir');
+		$composerDir = $vendorDir . '/composer';
+
+		$includeFiles = require $composerDir . '/autoload_files.php';
+		foreach ($includeFiles as $file) {
+			require $file;
+		}
+		// End of hack
+
 		/** @var \Composer\Package\Package $package */
 		$package = $composer->getPackage();
 
@@ -31,15 +43,26 @@ class Client
 
 		$extra = $package->getExtra();
 
-		$proxyUrl = Arry::get($extra, 'composer-proxy.url');
-		if ( ! $proxyUrl) {
+		$proxyUrls = (array)Arry::get($extra, 'composer-proxy.url');
+		if ( ! $proxyUrls) {
 			$io->write('<warning>No composer proxy url defined in composer.json. You might not use your proxy when fetching packages.</warning>');
 			return;
 		}
 
-		$client = new GuzzleClient([
-			'base_url' => $proxyUrl,
-		]);
-		$client->post('packages', $dependencies);
+		$io->write('<info>Sending data over to the defined proxies in order to cache your packages. Please wait, this may take a while...</info>');
+
+		$client = new GuzzleClient();
+
+		$requests = [];
+		foreach ($proxyUrls as $proxyUrl) {
+			$requests[] = $client->createRequest('POST', $proxyUrl.'/packages', [
+				'body' => $dependencies,
+			]);
+		}
+
+		$pool = new Pool($client, $requests);
+		$res = $pool->wait();
+
+		$io->write('<info>Proxies caching completed!</info>');
 	}
 }
